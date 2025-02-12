c1a6207f595f304cd14d5534add073343c375424
abhishekkenganal2 <abhishekkenganal6789@gmail.com>
2025-02-12 23:06:57 +0530

    Learnt JSX and BAbel

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello World!"),
-    React.createElement("h2", {}, "Hello I am H2!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello World!2"),
-    React.createElement("h2", {}, "Hello I am H2!2"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Hello World!"),
+//     React.createElement("h2", {}, "Hello I am H2!"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "Hello World!2"),
+//     React.createElement("h2", {}, "Hello I am H2!2"),
+//   ]),
+// ]);
+
+// Introduction to JSX and Babel
+const jsxhading = <h1>Hello World!</h1> // JSX for single element
+const jsx = (
+  <div>
+    <h1>Hello World!</h1>
+    <h2>Hello I am You</h2>
+  </div>
+); // JSX for multiple element
 
 const root = ReactDom.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(jsxhading)
